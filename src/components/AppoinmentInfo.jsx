f9f303da783e8da4e7ab750c8a4ed12349b142fe
271-noor <157873714+271-noor@users.noreply.github.com>
2024-03-06 15:04:49 +0530

    Add Event For Delete

--- src/components/AppoinmentInfo.jsx
+++ src/components/AppoinmentInfo.jsx
@@ -1,21 +1,29 @@
 import { BiTrash } from 'react-icons/bi'
 
 
-function AppoinmentInfo({appoinment}) {
+function AppoinmentInfo({appoinment, onDeleteAppoinment}) {
   return (
    
       <li className="px-3 py-3 flex items-start">
-          <button type="button" className="p-1.5 mr-1.5 mt-1 rounded-md bg-red-500 hover:bg-yellow-700">
+          <button 
+            onClick={() => onDeleteAppoinment(appoinment.id)}
+            type="button" 
+            className="p-1.5 mr-1.5 mt-1 rounded-md bg-red-500 hover:bg-yellow-700"
+          >
+
             <BiTrash />
+
           </button>
           <div className="flex-grow">
             <div className="flex items-center">
               <span className="flex-none font-medium text-2xl text-purple-500">
                   {appoinment.petName}
                 </span>
+
               <span className="flex-grow text-right">
                 Date: {appoinment.aptDate} Time: {appoinment.aptTime}
               </span>
+
             </div>
             <div >
                 <b className="font-bold text-purple-500"> Owner: </b>{appoinment.ownerName}
