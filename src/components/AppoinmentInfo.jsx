91da55430f5dbb74d0387dd859697c6f09a4328d
271-noor <157873714+271-noor@users.noreply.github.com>
2024-03-08 15:16:39 +0530

    modifyig

--- src/components/AppoinmentInfo.jsx
+++ src/components/AppoinmentInfo.jsx
@@ -4,7 +4,7 @@ import { BiTrash } from 'react-icons/bi'
 function AppoinmentInfo({appoinment, onDeleteAppoinment}) {
   return (
    
-      <li className="px-3 py-3 flex items-start">
+      <li className="px-3 py-3 flex items-start bg-cyan-500 bg-opacity-20">
           <button 
             onClick={() => onDeleteAppoinment(appoinment.id)}
             type="button" 
@@ -16,19 +16,19 @@ function AppoinmentInfo({appoinment, onDeleteAppoinment}) {
           </button>
           <div className="flex-grow">
             <div className="flex items-center">
-              <span className="flex-none font-medium text-2xl text-purple-500">
+              <span className="flex-none font-medium text-2xl text-cyan-400">
                   {appoinment.petName}
                 </span>
 
-              <span className="flex-grow text-right">
+              <span className="flex-grow text-right font-bold text-white">
                 Date: {appoinment.aptDate} Time: {appoinment.aptTime}
               </span>
 
             </div>
-            <div >
-                <b className="font-bold text-purple-500"> Owner: </b>{appoinment.ownerName}
+            <div  className="font-bold text-white">
+                <b className="font-bold text-white"> Owner: </b>{appoinment.ownerName}
               </div>
-            <div className="leading-tight">
+            <div className="leading-tight font-bold text-white">
               Message: {appoinment.message}
               </div>
           </div>
